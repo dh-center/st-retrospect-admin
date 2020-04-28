473f4b4399a205a0b4f7ebf9ccc0aeb6ec940f86
ilyamore88 <themorozik@gmail.com>
2020-04-28 23:53:38 +0300

    add router view

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './index.css';
 import Navigation from '../components/Navigation';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import routes from '../router/routes';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navigation />
+        <Navigation/>
+        <Switch>
+          {routes.map((route, key) => (
+            <Route
+              key={key}
+              path={route.path}
+              render={route.component}
+            />
+          ))}
+        </Switch>
       </Router>
     </div>
   );
---
+++ src/components/Quiz/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * Functional component for quiz view
+ *
+ * @returns {JSX.Element}
+ */
+export default function Quiz(): JSX.Element {
+  return (
+    <h1>Quiz</h1>
+  )
+}
