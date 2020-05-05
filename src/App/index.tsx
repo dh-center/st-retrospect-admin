98aa436f8b0fbff2ff5a743832d75174b01eefda
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-05 21:06:21 +0300

    get query for persons

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,14 +1,8 @@
 import React, { ReactElement } from 'react';
 import './index.css';
-// import Navigation from '../components/Navigation';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import routes from '../router/routes';
-import { QueryRenderer } from 'react-relay';
-import environment from '../relay-env';
-
-import graphql from 'babel-plugin-relay/macro';
-
-import { AppLocationsQuery } from './__generated__/AppLocationsQuery.graphql';
+import Navigation from '../components/Navigation';
+import { Switch, Route } from 'react-router-dom';
+import routes from '../router/routes';
 
 /**
  * Main component of the application
@@ -16,42 +10,17 @@ import { AppLocationsQuery } from './__generated__/AppLocationsQuery.graphql';
 function App(): ReactElement {
   return (
     <div className="App">
-      <QueryRenderer<AppLocationsQuery>
-        environment={environment}
-        query={graphql`
-          query AppLocationsQuery {
-            locations {
-              id
-              name
-            }
-          }
-        `}
-        variables={{}}
-        render={({ error, props }): React.ReactNode => {
-          if (error) {
-            return <div>Error!</div>;
-          }
-          if (!props) {
-            return <div>Loading...</div>;
-          }
 
-          return <div>User ID: {props.locations[0].name}</div>;
-        }}
-      >
-
-      </QueryRenderer>
-      {/* <Router>*/}
-      {/*  <Navigation/>*/}
-      {/*  <Switch>*/}
-      {/*    {routes.map((route, key) => (*/}
-      {/*      <Route*/}
-      {/*        key={key}*/}
-      {/*        path={route.path}*/}
-      {/*        render={route.component}*/}
-      {/*      />*/}
-      {/*    ))}*/}
-      {/*  </Switch>*/}
-      {/* </Router>*/}
+      <Navigation/>
+      <Switch>
+        {routes.map((route, key) => (
+          <Route
+            key={key}
+            path={route.path}
+            render={route.component}
+          />
+        ))}
+      </Switch>
     </div>
   );
 }
--- src/components/Persons/index.tsx
+++ src/components/Persons/index.tsx
@@ -1,12 +1,63 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
+import { QueryRenderer } from 'react-relay';
+import { PersonsQuery, PersonsQueryResponse } from './__generated__/PersonsQuery.graphql';
+import environment from '../../relay-env';
+import graphql from 'babel-plugin-relay/macro';
 
 /**
  * Functional component for persons view
  *
  * @returns {JSX.Element}
  */
-export default function Persons(): JSX.Element {
+export default function Persons(): ReactElement {
   return (
-    <h1>Persons</h1>
-  )
+    <QueryRenderer<PersonsQuery>
+      environment={environment}
+      query={graphql`
+          query PersonsQuery {
+            persons {
+              id
+              firstName
+              lastName
+              patronymic
+            }
+          }
+        `}
+      variables={{}}
+      render={({ error, props }): React.ReactNode => {
+        if (error) {
+          return <div>Error!</div>;
+        }
+        if (!props) {
+          return <div>Loading persons...</div>;
+        }
+
+        return <PersonsList persons={props.persons}/>;
+      }}
+    >
+
+    </QueryRenderer>
+  );
+}
+
+/**
+ * Component for displaying persons list
+ *
+ * @param props - react component props
+ */
+function PersonsList(props: {
+  persons: PersonsQueryResponse['persons'];
+}): ReactElement {
+  return <table>
+    <tbody>
+      {props.persons.map(person => {
+        return <tr key={person.id}>
+          <td>{person.id}</td>
+          <td>{person.firstName}</td>
+          <td>{person.lastName}</td>
+          <td>{person.patronymic}</td>
+        </tr>;
+      })}
+    </tbody>
+  </table>;
 }
