86292fdbe209b39518a49651b901d45742c32d87
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-09 19:39:01 +0300

    fix docs

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,4 @@
+/**
+ * How many entities will be on the page
+ */
 export const ENTITIES_PER_PAGE = 25;
--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,3 +1,6 @@
+/**
+ * Type definition for rc-pagination module
+ */
 declare module 'rc-pagination/lib/locale/ru_RU' {
   // eslint-disable-next-line
   export default object;
