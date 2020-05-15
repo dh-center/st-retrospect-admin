f71817e1844f8f89d63b47770769b85a400bad57
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-16 02:49:04 +0300

    rewrite to functional component

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,62 +1,60 @@
-import React, { FormEvent } from 'react';
+import React, { FormEvent, ReactElement, useState } from 'react';
 import authController from '../../authController';
 import { withRouter, RouteComponentProps } from 'react-router';
 import './index.css';
 
 /**
  * Component for authentication
+ *
+ * @param props - component props
  */
-class Login extends React.Component<RouteComponentProps> {
-  public state = {
-    email: '',
-    password: '',
-  }
-
-  /**
-   * Method for component rendering
-   */
-  public render(): React.ReactElement {
-    return (
-      <div className='login-page'>
-        <form className='login-page__form' onSubmit={(e): Promise<void> => this.handleLogin(e)}>
-          <h1 className='login-page__header'>Please, login</h1>
-          <div>
-            <input
-              className='login-page__input'
-              type="text"
-              value={this.state.email}
-              placeholder='Username'
-              onChange={(e): void => this.setState({ email: e.target.value })}
-            />
-          </div>
-          <div>
-            <input
-              className='login-page__input'
-              type="password"
-              placeholder='Password'
-              value={this.state.password}
-              onChange={(e): void => this.setState({ password: e.target.value })}
-            />
-
-          </div>
-          <button type="submit" className='login-page__submit-button'>
-            Enter
-          </button>
-        </form>
-
-      </div>
-    );
-  }
+function Login(props: RouteComponentProps): ReactElement {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   /**
    * Performs user login
    *
-   * @param e
+   * @param e - event to handle
    */
-  private async handleLogin(e: FormEvent<HTMLFormElement>): Promise<void> {
+  const handleLogin = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
-    await authController.login(this.state.email, this.state.password);
-    this.props.history.push('/persons');
-  }
+    await authController.login(email, password);
+    props.history.push('/persons');
+  };
+
+  return (
+    <div className='login-page'>
+      <form
+        className='login-page__form'
+        onSubmit={handleLogin}>
+        <h1 className='login-page__header'>Please, login</h1>
+        <div>
+          <input
+            className='login-page__input'
+            type="text"
+            value={email}
+            placeholder='Username'
+            onChange={(e): void => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className='login-page__input'
+            type="password"
+            placeholder='Password'
+            value={password}
+            onChange={(e): void => setPassword(e.target.value)}
+          />
+
+        </div>
+        <button type="submit" className='login-page__submit-button'>
+          Enter
+        </button>
+      </form>
+
+    </div>
+  );
 }
+
 export default withRouter(Login);
