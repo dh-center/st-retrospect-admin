6c117926089dbd51bdce4f674f1a7415f17af2d3
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-15 18:29:02 +0300

    attempt to decrypt secrets

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,4 +3,4 @@
 # Decrypt the file
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" \
---output $ENV_FILENAME .github/.env.gpg
+--output .env .github/$ENV_FILENAME
