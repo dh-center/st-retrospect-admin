cd2a61b1e620c40cd7618d9e89e8ed2fad9449f9
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-15 18:47:41 +0300

    debug

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+echo $ENV_FILENAME
+
 # Decrypt the file
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" \
---output .env .github/$ENV_FILENAME
+--output .env ".github/$ENV_FILENAME"
