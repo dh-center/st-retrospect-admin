256250a8223b492254d5a753eeda03e24cc6f496
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-15 18:01:09 +0300

    separate .env files for prod and stage

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,4 +3,4 @@
 # Decrypt the file
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" \
---output .env .github/.env.gpg
+--output $ENV_FILENAME .github/.env.gpg
