7453b5628784c5e975f6e6db4960c59cbebd8403
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-21 22:54:20 +0300

    fix eslint

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,17 +1,17 @@
-import React, {ReactElement} from 'react';
+import React, { ReactElement } from 'react';
 import Navigation from '../components/Navigation';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from '../components/Login';
 import PrivateRoute from '../components/PrivateRoute';
 import PersonsPage from '../components/Persons/PersonsPage';
 import QuestsPage from '../components/Quests/QuestsPage';
 import Quiz from '../components/Quiz';
-import {QueryRenderer} from 'react-relay';
+import { QueryRenderer } from 'react-relay';
 import environment from '../relay-env';
 import graphql from 'babel-plugin-relay/macro';
-import {AppQuery, AppQueryResponse} from './__generated__/AppQuery.graphql';
+import { AppQuery, AppQueryResponse } from './__generated__/AppQuery.graphql';
 
-const renderQuery = ({error, props}: { error: Error | null; props: AppQueryResponse | null }): React.ReactNode => {
+const renderQuery = ({ error, props }: { error: Error | null; props: AppQueryResponse | null }): React.ReactNode => {
   if (error) {
     return (
       <div>
@@ -65,10 +65,10 @@ function App(): ReactElement {
           <QueryRenderer<AppQuery>
             environment={environment}
             query={graphql`
-        query AppQuery {
-            ...Navigation_user
-        }
-      `}
+                query AppQuery {
+                    ...Navigation_user
+                }
+            `}
             variables={{}}
             render={renderQuery}/>
         </PrivateRoute>
