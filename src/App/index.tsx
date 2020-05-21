49a7e6da00c0eb3c5b4ff2ce33c98630f4f3877c
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-21 22:46:09 +0300

    change styles of login page

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,17 +1,17 @@
-import React, { ReactElement } from 'react';
+import React, {ReactElement} from 'react';
 import Navigation from '../components/Navigation';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import Login from '../components/Login';
 import PrivateRoute from '../components/PrivateRoute';
 import PersonsPage from '../components/Persons/PersonsPage';
 import QuestsPage from '../components/Quests/QuestsPage';
 import Quiz from '../components/Quiz';
-import { QueryRenderer } from 'react-relay';
+import {QueryRenderer} from 'react-relay';
 import environment from '../relay-env';
 import graphql from 'babel-plugin-relay/macro';
-import { AppQuery, AppQueryResponse } from './__generated__/AppQuery.graphql';
+import {AppQuery, AppQueryResponse} from './__generated__/AppQuery.graphql';
 
-const renderQuery = ({ error, props }: { error: Error | null; props: AppQueryResponse | null }): React.ReactNode => {
+const renderQuery = ({error, props}: { error: Error | null; props: AppQueryResponse | null }): React.ReactNode => {
   if (error) {
     return (
       <div>
@@ -24,39 +24,28 @@ const renderQuery = ({ error, props }: { error: Error | null; props: AppQueryRes
   }
 
   return (
-    <div className="app">
+    <>
+      <Navigation user={props}/>
+
       <Switch>
 
-        <Route path='/login'>
-          <Login/>
-        </Route>
-
-        <PrivateRoute path='/'>
-          <Navigation user={props}/>
-
-          <Switch>
-
-            <PrivateRoute path='/persons'>
-              <PersonsPage/>
-            </PrivateRoute>
-
-            <PrivateRoute path='/quests'>
-              <QuestsPage/>
-            </PrivateRoute>
-
-            <PrivateRoute path='/quiz'>
-              <Quiz/>
-            </PrivateRoute>
-
-            <PrivateRoute path='/'>
-              <Redirect to='/persons'/>
-            </PrivateRoute>
-          </Switch>
-
+        <PrivateRoute path='/persons'>
+          <PersonsPage/>
         </PrivateRoute>
 
+        <PrivateRoute path='/quests'>
+          <QuestsPage/>
+        </PrivateRoute>
+
+        <PrivateRoute path='/quiz'>
+          <Quiz/>
+        </PrivateRoute>
+
+        <PrivateRoute path='/'>
+          <Redirect to='/persons'/>
+        </PrivateRoute>
       </Switch>
-    </div>
+    </>
   );
 };
 
@@ -65,15 +54,28 @@ const renderQuery = ({ error, props }: { error: Error | null; props: AppQueryRes
  */
 function App(): ReactElement {
   return (
-    <QueryRenderer<AppQuery>
-      environment={environment}
-      query={graphql`
+    <div className="h-100">
+      <Switch>
+
+        <Route path='/login'>
+          <Login/>
+        </Route>
+
+        <PrivateRoute path='/'>
+          <QueryRenderer<AppQuery>
+            environment={environment}
+            query={graphql`
         query AppQuery {
             ...Navigation_user
         }
       `}
-      variables={{}}
-      render={renderQuery}/>
+            variables={{}}
+            render={renderQuery}/>
+        </PrivateRoute>
+
+      </Switch>
+    </div>
+
   );
 }
 
