43c7e9673a0dcb731e1be83e315b936186889ef3
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-23 21:07:52 +0300

    add editorjs to Quest info page

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -5,3 +5,10 @@ declare module 'rc-pagination/lib/locale/ru_RU' {
   // eslint-disable-next-line
   export default object;
 }
+
+declare module '@editorjs/header';
+declare module '@editorjs/list';
+declare module '@editorjs/image';
+declare module '@editorjs/delimiter';
+declare module '@editorjs/marker';
+declare module '@editorjs/quote';
