1a90f3b8828b975865a811ed472527374ddf77ff
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-29 13:34:20 +0300

    base barplot

---
+++ src/components/Visualization/PersonsBirthDatesBarplot.tsx
@@ -0,0 +1,116 @@
+import React, { useEffect } from 'react';
+import * as d3 from 'd3';
+
+/**
+ * Displays barplot
+ *
+ * @param props - component props for rendering
+ */
+export default function PersonsBirthDatesBarplot(props: {
+  readonly dates: string[];
+}): React.ReactElement {
+  useEffect(() => {
+    /**
+     * Set the dimensions and margins of the graph
+     */
+    const margin = {
+      top: 10,
+      right: 30,
+      bottom: 90,
+      left: 40,
+    };
+    const width = 800 - margin.left - margin.right;
+    const height = 450 - margin.top - margin.bottom;
+
+    const groupedByDates = props.dates.reduce<Record<number, number>>((acc, curr) => {
+      const birthYearString = curr.match(/\d{4}/)?.shift();
+      const birthYear = birthYearString && +birthYearString;
+
+      if (birthYear) {
+        if (acc[birthYear]) {
+          acc[birthYear]++;
+        } else {
+          acc[birthYear] = 1;
+        }
+      }
+
+      return acc;
+    }, {});
+
+    const numberKeys = Object.keys(groupedByDates).map(key => +key);
+
+    const YEARS_IN_PERIOD = 20;
+    const minBirthYear = Math.min(...numberKeys);
+
+    const getPeriodNumber = (year: number): number => Math.floor((year - minBirthYear) / YEARS_IN_PERIOD);
+    const getPeriodStartDate = (periodNumber: number): number => minBirthYear + YEARS_IN_PERIOD * periodNumber;
+
+    const groupedByPeriods = numberKeys.reduce<Record<number, number>>((acc, val) => {
+      const periodNumber = getPeriodNumber(val);
+      const periodStartDate = getPeriodStartDate(periodNumber);
+
+      if (acc[periodStartDate]) {
+        acc[periodStartDate] += groupedByDates[val];
+      } else {
+        acc[periodStartDate] = groupedByDates[val];
+      }
+
+      return acc;
+    }, {});
+
+    const maxCount = Math.max(...Object.values(groupedByPeriods));
+
+    /**
+     * Append the svg object to the body of the page
+     */
+    const svg = d3.select('#my_dataviz')
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform',
+        'translate(' + margin.left + ',' + margin.top + ')');
+
+    /**
+     * X axis
+     */
+    const x = d3.scaleBand()
+      .range([0, width])
+      .domain(Object.keys(groupedByPeriods))
+      .padding(0.2);
+
+    svg.append('g')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(d3.axisBottom(x))
+      .selectAll('text')
+      .attr('transform', 'translate(-10,0) rotate(-45)')
+      .style('text-anchor', 'end');
+
+    /**
+     * Y axis
+     */
+    const y = d3.scaleLinear()
+      .domain([0, maxCount])
+      .range([height, 0]);
+
+    svg.append('g')
+      .call(d3.axisLeft(y));
+
+    /**
+     * Bars
+     */
+    svg.selectAll('mybar')
+      .data(Object.keys(groupedByPeriods))
+      .enter()
+      .append('rect')
+      .attr('x', function (d) {
+        return x(d) || 0;
+      })
+      .attr('width', x.bandwidth())
+      .attr('fill', '#69b3a2')
+      .attr('height', (d) => height - y(groupedByPeriods[+d]))
+      .attr('y', (d) => y(groupedByPeriods[+d]));
+  }, [ props.dates ]);
+
+  return <div id='my_dataviz'/>;
+}
--- src/components/Visualization/index.tsx
+++ src/components/Visualization/index.tsx
@@ -1,14 +1,59 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { QueryRenderer } from 'react-relay';
+import environment from '../../relay-env';
+import graphql from 'babel-plugin-relay/macro';
+import { VisualizationPageQuery, VisualizationPageQueryResponse } from './__generated__/VisualizationPageQuery.graphql';
+import PersonsBirthDatesBarplot from './PersonsBirthDatesBarplot';
 
 /**
  *
  */
 export default function VisualizationPage(): React.ReactElement {
+  ;
+
   return (
     <div>
       <h1>Visualization</h1>
       <NavLink to={'/'}>Back</NavLink>
+      <QueryRenderer<VisualizationPageQuery>
+        environment={environment}
+        query={graphql`
+          query VisualizationPageQuery {
+            persons {
+              edges {
+                node {
+                  birthDate
+                  deathDate
+                }
+              }
+            }
+          }
+        `}
+        variables={{}}
+        render={({ error, props }: { error: Error | null; props: VisualizationPageQueryResponse | null }): React.ReactNode => {
+          if (error) {
+            return (
+              <div>
+                <pre>
+                  Error during loading page
+                  {error.toString()}
+                </pre>
+              </div>
+            );
+          }
+
+          if (!props) {
+            return <div>Loading</div>;
+          }
+
+          return (
+            <PersonsBirthDatesBarplot
+              dates={props.persons.edges.map(edge => edge.node.birthDate).filter(Boolean) as string[]}
+            />
+          );
+        }}
+      />
     </div>
   );
 }
