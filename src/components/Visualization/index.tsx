4e911d82dc01427c135ae92524a5ae2880966e6b
Nikita Melnikov <nikmel2803@gmail.com>
2020-06-21 20:52:37 +0300

    create slide component

--- src/components/Visualization/index.tsx
+++ src/components/Visualization/index.tsx
@@ -8,6 +8,7 @@ import PersonsBirthDatesBarplot from './PersonsBirthDatesBarplot';
 import PersonsLifeYearsDiagram from './PersonsLifeYearsDiagram';
 import RelationsGraph from './RelationsGraph';
 import './index.css';
+import { Slide } from './Slide';
 
 /**
  * Page with plots for visualisation of Database content
@@ -109,67 +110,25 @@ export default function VisualizationPage(): React.ReactElement {
           return (
             <Switch>
               <Route path={'/visualization/1'}>
-                <NavLink className="visualization-page__prev-link" to={'/visualization/3'}>
-                  <svg className="bi bi-arrow-left" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd"
-                      d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"/>
-                    <path fillRule="evenodd" d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                  </svg>
-                </NavLink>
-                <PersonsBirthDatesBarplot
-                  dates={props.persons.edges.map(edge => edge.node.birthDate)}
-                />
-                <NavLink className="visualization-page__next-link" to={'/visualization/2'}>
-                  <svg className="bi bi-arrow-right" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd"
-                      d="M10.146 4.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L12.793 8l-2.647-2.646a.5.5 0 0 1 0-.708z"/>
-                    <path fillRule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5H13a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8z"/>
-                  </svg>
-                </NavLink>
+                <Slide>
+                  <PersonsBirthDatesBarplot
+                    dates={props.persons.edges.map(edge => edge.node.birthDate)}
+                  />
+                </Slide>
               </Route>
               <Route path={'/visualization/2'}>
-                <NavLink className="visualization-page__prev-link" to={'/visualization/1'}>
-                  <svg className="bi bi-arrow-left" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd"
-                      d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"/>
-                    <path fillRule="evenodd" d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                  </svg>
-                </NavLink>
-                <PersonsLifeYearsDiagram
-                  persons={props.persons.edges.map(edge => edge.node)}
-                />
-                <NavLink className="visualization-page__next-link" to={'/visualization/3'}>
-                  <svg className="bi bi-arrow-right" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd"
-                      d="M10.146 4.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L12.793 8l-2.647-2.646a.5.5 0 0 1 0-.708z"/>
-                    <path fillRule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5H13a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8z"/>
-                  </svg>
-                </NavLink>
+                <Slide>
+                  <PersonsLifeYearsDiagram
+                    persons={props.persons.edges.map(edge => edge.node)}
+                  />
+                </Slide>
               </Route>
               <Route path={'/visualization/3'}>
-                <NavLink className="visualization-page__prev-link" to={'/visualization/2'}>
-                  <svg className="bi bi-arrow-left" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd"
-                      d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"/>
-                    <path fillRule="evenodd" d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                  </svg>
-                </NavLink>
-                <RelationsGraph
-                  data={props}
-                />
-                <NavLink className="visualization-page__next-link" to={'/visualization/1'}>
-                  <svg className="bi bi-arrow-right" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd"
-                      d="M10.146 4.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L12.793 8l-2.647-2.646a.5.5 0 0 1 0-.708z"/>
-                    <path fillRule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5H13a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8z"/>
-                  </svg>
-                </NavLink>
+                <Slide>
+                  <RelationsGraph
+                    data={props}
+                  />
+                </Slide>
               </Route>
             </Switch>
           );
