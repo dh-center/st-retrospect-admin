f89b6fac699f26c6389be308b815d0ccc87c0aab
Nikita Melnikov <nikmel2803@gmail.com>
2020-08-29 12:47:43 +0300

    fix docs

--- src/components/Locations/LocationInfo.tsx
+++ src/components/Locations/LocationInfo.tsx
@@ -2,26 +2,14 @@ import React from 'react';
 import { EntityInfoComponentProps, Location, OmitId } from '../../types/entities';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
-import Form from 'react-bootstrap/Form';
 import LocationInstanceInfo from './LocationInstanceInfo';
 
 /**
- * Generates empty quest
- */
-export function generateLocation(): OmitId<Location> {
-  return {
-    instances: [],
-  };
-}
-
-/**
- * Component of quest fields
+ * Displays information about location
  *
- * @param props - props of component
+ * @param props - props for component rendering
  */
 export default function LocationInfo(props: EntityInfoComponentProps<OmitId<Location>>): React.ReactElement {
-  const onChange = props.onChange || ((e: OmitId<Location>): void => { /* do nothing */ });
-
   return (
     <div>
       <Tabs>
