b63ea90c5a480fcd196f26225309cf9c31e92abe
Nikita Melnikov <nikmel2803@gmail.com>
2020-08-29 12:22:03 +0300

    LocationView without HOC

--- src/components/Locations/LocationView.tsx
+++ src/components/Locations/LocationView.tsx
@@ -1,34 +1,64 @@
-import makeViewPage from '../Entities/makeViewPage';
-import LocationInfo from './LocationInfo';
+import React from 'react';
 import graphql from 'babel-plugin-relay/macro';
+import { QueryRenderer } from 'react-relay';
+import environment from '../../relay-env';
+import { useParams } from 'react-router';
+import { LocationViewQuery } from './__generated__/LocationViewQuery.graphql';
+import LocationInfo from './LocationInfo';
 
-const LocationViewComponent = makeViewPage(
-  LocationInfo,
-  graphql`
-    query LocationViewQuery($id: ID!) {
-      entity: location(id: $id) {
-        id
-        instances {
-          id
-          name
-          demolitionDate
-          constructionDate
-          architects {
+/**
+ * Page with location info to view
+ */
+function LocationView(): React.ReactElement {
+  const { id } = useParams();
+
+  return (
+    <QueryRenderer<LocationViewQuery>
+      environment={environment}
+      query={graphql`
+        query LocationViewQuery($id: ID!) {
+          entity: location(id: $id) {
             id
-            firstName
-            lastName
-            patronymic
-          }
-          startDate
-          endDate
-          description
-          locationTypes {
-            id
-            name
+            instances {
+              id
+              name
+              demolitionDate
+              constructionDate
+              architects {
+                id
+                firstName
+                lastName
+                patronymic
+              }
+              startDate
+              endDate
+              description
+              locationTypes {
+                id
+                name
+              }
+            }
           }
         }
-      }
-    }`
-);
+      `}
+      variables={{ id }}
+      render={({ error, props }) => {
+        if (error) {
+          return <div>Error</div>;
+        }
 
-export default LocationViewComponent;
+        if (!props) {
+          return <div>Loading</div>;
+        }
+
+        if (!props.entity) {
+          return <div>There is no location with provided id</div>;
+        }
+
+        return <LocationInfo entity={props.entity}/>;
+      }}
+    />
+  );
+}
+
+export default LocationView;
