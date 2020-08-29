f8d8a4ddb6e6452a6c66b9a7543b1dfc4671ca52
Nikita Melnikov <nikmel2803@gmail.com>
2020-08-29 12:27:02 +0300

    remove LocationCreate.tsx

--- src/components/Locations/index.tsx
+++ src/components/Locations/index.tsx
@@ -1,7 +1,6 @@
 import React, { ReactElement } from 'react';
 import { Switch } from 'react-router-dom';
 import PrivateRoute from '../PrivateRoute';
-import LocationCreate from './LocationCreate';
 import LocationView from './LocationView';
 import LocationPage from './LocationsPage';
 
@@ -11,9 +10,6 @@ import LocationPage from './LocationsPage';
 export default function LocationsRouter(): ReactElement {
   return (
     <Switch>
-      <PrivateRoute path={'/locations/create'}>
-        <LocationCreate/>
-      </PrivateRoute>
       <PrivateRoute path={'/locations/:id'}>
         <LocationView/>
       </PrivateRoute>
