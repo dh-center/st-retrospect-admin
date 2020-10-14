42a9b1720f2ecdae07b5c54e27903cfe11480d2b
ilyamore88 <themorozik@gmail.com>
2020-10-14 15:09:24 +0300

    create env file in workflow

--- .github/scripts/decrypt_secret.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-# --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" \
---output .env .github/.env.gpg
