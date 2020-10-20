0d39b8df0e4903947ab5a1ff7f64b0f6f46fa7f4
Nikita Melnikov <nikmel2803@gmail.com>
2020-10-21 01:08:12 +0300

    update docs

--- src/components/Locations/LocationEditForm.tsx
+++ src/components/Locations/LocationEditForm.tsx
@@ -40,10 +40,21 @@ export function updateInfo(input: UpdateLocationInput): Promise<LocationEditForm
   });
 }
 
+/**
+ * Props for LocationEditForm
+ */
 interface Props {
+  /**
+   * Location to edit
+   */
   originalLocation: LocationEditForm_originalLocation;
 }
 
+/**
+ * Form for updating location information
+ *
+ * @param props - props for component rendering
+ */
 function LocationEditForm(props: Props): React.ReactElement {
   const [input, setInput] = useState<UpdateLocationInput>({
     ...props.originalLocation,
