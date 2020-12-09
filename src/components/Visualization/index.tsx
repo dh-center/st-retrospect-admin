78155e66896c4c9332bff5b8f9aed76312a9cf4b
Nikita Melnikov <nikmel2803@gmail.com>
2020-12-09 15:53:58 +0300

    add locations map and hide persons tree map

--- src/components/Visualization/index.tsx
+++ src/components/Visualization/index.tsx
@@ -10,7 +10,6 @@ import RelationsGraph from './RelationsGraph';
 import { Slide } from './Slide';
 import './index.css';
 import GenderDistribution from './GenderDistribution';
-import PersonsTreeMap from './PersonsTreeMap';
 import LoadingPlaceholder from '../utils/LoadingPlaceholder';
 
 /**
@@ -61,9 +60,11 @@ export default function VisualizationPage(): React.ReactElement {
           viewBox='0 0 16 16'
           width='1em'
           xmlns='http://www.w3.org/2000/svg'>
-          <path d='M5.854 5.646a.5.5 0 0 1 0 .708L3.207 9l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z'
+          <path
+            d='M5.854 5.646a.5.5 0 0 1 0 .708L3.207 9l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z'
             fillRule='evenodd'/>
-          <path d='M13.5 2.5a.5.5 0 0 1 .5.5v4a2.5 2.5 0 0 1-2.5 2.5H3a.5.5 0 0 1 0-1h8.5A1.5 1.5 0 0 0 13 7V3a.5.5 0 0 1 .5-.5z'
+          <path
+            d='M13.5 2.5a.5.5 0 0 1 .5.5v4a2.5 2.5 0 0 1-2.5 2.5H3a.5.5 0 0 1 0-1h8.5A1.5 1.5 0 0 0 13 7V3a.5.5 0 0 1 .5-.5z'
             fillRule='evenodd'/>
         </svg>
       </NavLink>
@@ -86,7 +87,10 @@ export default function VisualizationPage(): React.ReactElement {
             ...PersonsTreeMap_data
           }
         `}
-        render={({ error, props }: { error: Error | null; props: VisualizationPageQueryResponse | null }): React.ReactNode => {
+        render={({
+          error,
+          props,
+        }: { error: Error | null; props: VisualizationPageQueryResponse | null }): React.ReactNode => {
           if (error) {
             return (
               <div>
@@ -123,9 +127,21 @@ export default function VisualizationPage(): React.ReactElement {
             'genders-distribution': (
               <GenderDistribution persons={props.persons.edges.map(edge => edge.node)}/>
             ),
-            'persons-tree-map': (
-              <PersonsTreeMap data={props}/>
+            'locations-map': (
+              <div className='visualization-block'>
+                <h2 className='visualization-block__header'>Locations map</h2>
+                <div className='visualization-block__content visualization-block__content--light p-0'>
+                  <iframe
+                    frameBorder='0'
+                    height='500'
+                    src='https://observablehq.com/embed/@nikmel/st-retrospect-locations-map?cell=map'
+                    width='100%'></iframe>
+                </div>
+              </div>
             ),
+            // 'persons-tree-map': (
+            //   <PersonsTreeMap data={props}/>
+            // ),
           };
 
           return (
@@ -144,7 +160,7 @@ export default function VisualizationPage(): React.ReactElement {
                   );
                 })
               }
-              <Redirect to='/visualization/birth-dates-barplot' />
+              <Redirect to='/visualization/birth-dates-barplot'/>
             </Switch>
           );
         }}
@@ -159,7 +175,8 @@ export default function VisualizationPage(): React.ReactElement {
               viewBox='0 0 16 16'
               width='1em'
               xmlns='http://www.w3.org/2000/svg'>
-              <path d='M5.5 0a.5.5 0 0 1 .5.5v4A1.5 1.5 0 0 1 4.5 6h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5zm5 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 10 4.5v-4a.5.5 0 0 1 .5-.5zM0 10.5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 6 11.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zm10 1a1.5 1.5 0 0 1 1.5-1.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4z'
+              <path
+                d='M5.5 0a.5.5 0 0 1 .5.5v4A1.5 1.5 0 0 1 4.5 6h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5zm5 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 10 4.5v-4a.5.5 0 0 1 .5-.5zM0 10.5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 6 11.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zm10 1a1.5 1.5 0 0 1 1.5-1.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4z'
                 fillRule='evenodd'/>
             </svg>
           )
@@ -170,7 +187,8 @@ export default function VisualizationPage(): React.ReactElement {
               viewBox='0 0 16 16'
               width='1em'
               xmlns='http://www.w3.org/2000/svg'>
-              <path d='M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z'
+              <path
+                d='M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z'
                 fillRule='evenodd'/>
             </svg>
           )}
