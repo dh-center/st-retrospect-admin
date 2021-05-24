f0022a60f25e75142b7d53465db5c05a9a05a1f6
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2021-05-25 00:29:45 +0300

    fixed svg icon color

--- src/editorjs-plugins/ApproximationToCoordinates.tsx
+++ src/editorjs-plugins/ApproximationToCoordinates.tsx
@@ -164,8 +164,8 @@ export default class ApproximationToCoordinatesConstructor implements BlockTool 
    */
   public static get toolbox(): ToolboxConfig {
     return {
-      icon: `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M20 0L0 8.57143L8.23529 11.7647L11.4286 20L20 0ZM11.5212 14.7067L9.78102 10.219L5.29326 8.47882L16.1921 3.80789L11.5212 14.7067Z" fill="black"/>
+      icon: `<svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M20 0L0 8.57143L8.23529 11.7647L11.4286 20L20 0ZM11.5212 14.7067L9.78102 10.219L5.29326 8.47882L16.1921 3.80789L11.5212 14.7067Z"/>
             </svg>`,
       title: 'Approach to coordinates',
     };
